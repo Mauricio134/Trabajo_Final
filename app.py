71265d6112b226c06818920ce8c9b1455e282133
Mauricio132004 <mauriciodai14@gmail.com>
2022-05-20 04:13:08 -0500

    addbrand

--- app.py
+++ app.py
@@ -8,23 +8,20 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/amazon"
 mongo = PyMongo(app)
 app.config['SECRET_KEY'] = 'hello'
-
 app.permanent_session_lifetime = timedelta(hours = 3)
 
-
-@app.route('/', defaults={'id': ""})
-@app.route('/<id>')
-def home(id):
+@app.route('/')
+def home():
     if "user" in session:
         user = session["user"]
         productos = mongo.db.products.find()
-        return render_template("menu.html", productos = productos)
+        users = mongo.db.users.find()
+        return render_template("menu.html", productos = productos, users = users)
     else:
         return redirect(url_for("login"))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    id_ = ""
     if request.method == 'POST':
         session.permanent = True
         email = request.form.get('email')
@@ -43,17 +40,15 @@ def login():
             else:
                 Usuario = False
                 Contrasena = False
-        if Usuario == True:
-            if Contrasena == True:
-                flash("Usuario Logueado Correctamente", category='success')
-                return redirect(url_for('home'))
+        if Usuario == True and Contrasena == True:
+            flash("Usuario Logueado Correctamente", category='success')
+            return redirect(url_for('home'))
         else:
             flash("Datos Incorrectos. Intentalo nuevamente", category='error')
         return render_template("login.html")
     else:
         if "user" in session:
-            print("a=" , session["user"])
-            return redirect(url_for("home", id=id_ ))
+            return redirect(url_for("home"))
         return render_template("login.html")
 
 @app.route('/logout')
@@ -96,38 +91,47 @@ def signup():
             id = mongo.db.users.insert_one(
             {'username': username, 'email': email, 'password': hash_password, 's_password': hash_s_password}
             )
-            response = {
-                'id': str(id),
-                'username': username,
-                'email': email,
-                'password': hash_password,
-                's_password': hash_s_password
-            }
             flash("Cuenta Creada con Exito!!", category = 'success')
             return redirect(url_for('home'))
+        return render_template("register.html")
     else:
         if "user" in session:
             return redirect(url_for('home'))
         return render_template("register.html")
 
-
 @app.route('/addbrand', methods=['GET', 'POST'])
 def addbrand():
     if request.method == 'POST':
+        contra = request.form.get('contra')
         productname = request.form.get('productname')
         prize = request.form.get('prize')
         discount = request.form.get('discount')
         description = request.form.get('description')
         image = request.form.get('image')
-        id = mongo.db.products.insert_one({
-            'name': productname, 
-            'prize': prize, 
-            'discount': discount, 
-            'description': description,
-            'file': image
-        })
+        users = mongo.db.users.find()
+        user_id = None
+        for user in users:
+            hashing = check_password_hash(user['password'], contra)
+            if hashing:
+                user_id = user['_id']
+        if user_id == None:
+            flash("No eres el usuario", category='error')
+            return redirect(url_for('addbrand'))
+        else:
+            id = mongo.db.products.insert_one({
+                'name': productname, 
+                'prize': prize, 
+                'discount': discount, 
+                'description': description,
+                'file': image,
+                'user_id': user_id
+            })
         return redirect(url_for('home'))
     return render_template('addbrand.html')
 
+@app.route('/delete/<id>')
+def delete(id):
+    productos = mongo.db.products.find_one_and_delete({"_id": ObjectId(id)})
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(debug = True)
